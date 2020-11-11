0095af189e7b91801c9727bba2ecde2e788902ae
Dhirendra-byte <68893214+Dhirendra-byte@users.noreply.github.com>
2020-11-11 23:33:24 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
 import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
-
 const App = () => {
   return (
-    <Welcome name="" />
-  )
-}
-
-
+    <Welcome name="Dhirendra Kumar Patel" />
+)}
 export default App;
